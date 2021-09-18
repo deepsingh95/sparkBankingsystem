837738472f47c6fb457fc1e0929e6c6cd4c0200a
Deepak Singh <ds788128@gmail.com>
2021-09-18 11:00:37 +0530

    ---

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static('PUBLIC'))
+app.use(express.static('public'))
 
 app.use(require('./router/index'))
 
